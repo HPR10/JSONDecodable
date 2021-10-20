4f5ad9120593627ccb67bc82033aeabdd60e0bcc
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-10-20 17:31:19 -0300

    Simples Decodificação JSON

--- JSONDecoding/ViewController.swift
+++ JSONDecoding/ViewController.swift
@@ -11,9 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        get()
     }
 
-
+   func get()
+    {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1/comments")!
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            do {
+                let users = try JSONDecoder().decode([Users].self, from: data!)
+                for user in users
+                {
+                   print("ID: \(user.id), title: \(user.name), email: \(user.email), body: \(user.body)")
+                }
+            }
+            catch
+            {
+                print("Error ao obter os dados")
+            }
+        }.resume()
+    }
 }
 
+struct Users: Decodable
+{
+   let id: Int
+   let name: String
+   let email: String
+   let body:String
+}
